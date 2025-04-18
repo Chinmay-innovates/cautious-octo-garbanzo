fc2c12039aedfe74a38faa3ff10efe3141624251
Chinmay-innovates <chinmaygope69@gmail.com>
2025-04-18 06:15:41 +0530

    customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Home() {
-	return (
-		<div>
-			<p className="text-rose-500">Hello world</p>
-		</div>
-	);
+  return (
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant={"elevated"}>Click me</Button>
+        </div>
+        <div>
+          <Input placeholder="Type here..." />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="Type here..." />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
+    </div>
+  );
 }
