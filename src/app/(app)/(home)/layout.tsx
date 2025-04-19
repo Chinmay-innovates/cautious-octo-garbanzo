b4fc77eb93064c15b45c0cbc5f693539cf7ed05b
Chinmay-innovates <chinmaygope69@gmail.com>
2025-04-19 14:21:59 +0530

    Implement category management with sidebar and search filters

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -5,6 +5,7 @@ import { Footer } from "./footer";
 import { Navbar } from "./navbar";
 import { SearchFilters } from "./search-filters";
 import { Category } from "@/payload-types";
+import { CustomCategory } from "./types";
 
 interface Props {
 	children: React.ReactNode;
@@ -22,9 +23,10 @@ const Layout = async ({ children }: Props) => {
 				exists: false,
 			},
 		},
+		sort: "name",
 	});
 
-	const formattedData = data.docs.map((doc) => ({
+	const formattedData: CustomCategory[] = data.docs.map((doc) => ({
 		...doc,
 		subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
 			// due to "depth: 1" above, this is always a Category
@@ -32,8 +34,6 @@ const Layout = async ({ children }: Props) => {
 			subcategories: undefined,
 		})),
 	}));
-
-	console.log({ data, formattedData });
 
 	return (
 		<div className="flex flex-col min-h-screen">
