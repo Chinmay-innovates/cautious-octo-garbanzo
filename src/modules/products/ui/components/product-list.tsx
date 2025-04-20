6679c8055bca4ce82ef1451de4cbd407c4cba915
Chinmay-innovates <chinmaygope69@gmail.com>
2025-04-20 17:12:36 +0530

    feat(products): add price filtering functionality and enhance product listing UI

--- src/modules/products/ui/components/product-list.tsx
+++ src/modules/products/ui/components/product-list.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Card, CardDescription, CardTitle } from '@/components/ui/card';
 import { useTRPC } from '@/trpc/client';
 import { useSuspenseQuery } from '@tanstack/react-query';
 
@@ -13,7 +14,17 @@ export const ProductList = ({ categorySlug }: Props) => {
       categorySlug,
     }),
   );
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <Card>
+      {data.docs.map((product) => (
+        <div key={product.id} className="p-2 border-b last:border-b-0">
+          <CardTitle>{product.name}</CardTitle>
+          <CardDescription className="text-xl">{product.description}</CardDescription>
+          <p className="text-gray-500">$ {product.price}</p>
+        </div>
+      ))}
+    </Card>
+  );
 };
 
 export const ProductListSkeleton = () => {
