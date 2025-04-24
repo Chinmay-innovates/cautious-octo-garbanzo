f57f0afdb61baa9ad059e8a942c9df6c05c55141
Chinmay-innovates <chinmaygope69@gmail.com>
2025-04-24 13:30:05 +0530

    feat: implement checkout functionality with new components and hooks

--- src/modules/tenants/ui/components/footer.tsx
+++ src/modules/tenants/ui/components/footer.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 export const Footer = () => {
   return (
     <footer className="border-t font-medium bg-white pt-1">
-      <div className="max-w-(--breakpoint-xl) mx-auto flex items-center gap-2 h-full px-4 lg:px-12">
+      <div className="max-w-(--breakpoint-xl) mx-auto flex items-center gap-2 h-full px-4 py-6 lg:px-12">
         <p>Powered by</p>
         <Link href={'/'}>
           <span className={cn('font-semibold text-2xl', poppins.className)}>funroad</span>
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,15 +1,19 @@
 import { createTRPCRouter } from '../init';
 import { authRouter } from '@/modules/auth/server/procedures';
+import { tagsRouter } from '@/modules/tags/server/procedures';
 import { productsRouter } from '@/modules/products/server/procedures';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
-import { tagsRouter } from '@/modules/tags/server/procedures';
 import { tenantsRouter } from '@/modules/tenants/server/procedures';
+import { checkoutRouter } from '@/modules/checkout/server/procedures';
+
 export const appRouter = createTRPCRouter({
   categories: categoriesRouter,
   products: productsRouter,
   tenants: tenantsRouter,
+  checkout: checkoutRouter,
   auth: authRouter,
   tags: tagsRouter,
 });
+
 // export type definition of API
 export type AppRouter = typeof appRouter;
